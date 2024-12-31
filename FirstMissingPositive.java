4ad34a4ab6f67e6723c1b4156bdef590f332b484
Kamal Singh <113432856+Kamal-Singh22@users.noreply.github.com>
2024-12-31 09:44:00 +0000

    Added the First Missing Positive Number Problem

---
+++ FirstMissingPositive.java
@@ -0,0 +1,36 @@
+public class FirstMissingPositive {
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+    
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n + 1;
+            }
+        }
+
+       
+        for (int i = 0; i < n; i++) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+
+      
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+    
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -1, 1};
+        System.out.println("Input: [3, 4, -1, 1]");
+        System.out.println("First Missing Positive: " + firstMissingPositive(nums));
+    }
+}
